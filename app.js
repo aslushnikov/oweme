56263cdfef6d23c6ed8e7f7f3ceee05234d7c490
Andrey Lushnikov <aslushnikov@gmail.com>
2014-01-13 12:04:00 +0400

    add gzip compression

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ function setUpServer(database)
     app.set('port', process.env.PORT || 3000);
     app.set('views', __dirname + '/views');
     app.set('view engine', 'jade');
+    app.use(express.compress());
     app.use(express.favicon());
     app.use(express.logger('dev'));
     app.use(express.bodyParser());
