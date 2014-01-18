5cd4df590e9249bef43d137668fe3c0fea9e0f52
Andrey Lushnikov <aslushnikov@gmail.com>
2014-01-18 12:57:43 +0400

    significantly reduce clear-database task

--- tasks/clear-database.js
+++ tasks/clear-database.js
@@ -5,39 +5,12 @@ var orm = require("orm")
 module.exports = function()
 {
     return database.connect()
-    // ensuring DB has some dummy entries
-    .then(dropModels)
+    .then(function(db) {
+        return Q.denodeify(db.drop.bind(db))()
+        .finally(db.close.bind(db));
+    })
     .fail(function(err) {
         throw err;
-    });
-}
-
-function dropModels(db)
-{
-    var removePromises = [];
-    for (var modelName in db.models) {
-        var model = db.models[modelName];
-        var drop = Q.denodeify(model.drop.bind(model));
-        var promise = drop()
-        .then(function(modelName) {
-            console.log("Model " + modelName + " successfully removed.");
-        }.bind(this, modelName))
-        .fail(function(modelName, error) {
-            console.error("Failed to remove model " + modelName);
-            throw error;
-        }.bind(this, modelName));
-        removePromises.push(promise);
-    }
-    Q.all(removePromises)
-    .then(function() {
-        console.log("Database cleared.");
-    })
-    .fail(function(error) {
-        console.error("Failed to clear database");
-        throw error;
-    })
-    .finally(function() {
-        db.close();
     });
 }
 
