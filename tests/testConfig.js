802666ec2003d0d6a6a5b6d0670426f3879bdc9c
Andrey Lushnikov <lushnikov@chromium.org>
2014-01-25 04:15:09 +0400

    add travis for fun & profit

--- tests/testConfig.js
+++ tests/testConfig.js
@@ -1,6 +1,6 @@
 module.exports = {
     database: {
-        database : "test",
+        database : "oweme_test",
         protocol : "mysql",
         host     : "127.0.0.1",
         port     : 3306,         // optional, defaults to database default
